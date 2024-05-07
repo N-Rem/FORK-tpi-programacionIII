f804cf726b137be7b0d960aa64d310f3cf48e460
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-05-07 19:42:32 -0300

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Client : User
+    {
+        public Client(int Id, string Name,string Password) 
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Password = Password;
+            this.IsClient = true;
+        }
+    }
+}
---
+++ Domain/Entities/Sneaker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Sneaker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
