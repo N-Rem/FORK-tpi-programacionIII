57ae52b66bfa686bb4fdcd4a817d72c7a2a3ec91
N-Rem <nicolasromero.barrios@gmail.com>
2024-06-03 13:49:19 -0300

    Add Entities and library class

---
+++ Domain/Entities/Reservation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Reservation
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        //de uno a muchos
+        [Required]
+        public ICollection<Sneaker> Sneakers { get; set; } = new List<Sneaker>();
+
+        //De uno a muchos
+        [ForeignKey("Client")]
+        public int ClientId {  get; set; }
+
+        [Required]
+        public User Client { get; set; }
+    }
+}
--- Domain/Entities/Sneaker.cs
+++ Domain/Entities/Sneaker.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,31 @@ namespace Domain.Entities
 {
     public class Sneaker
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Brand { get; set; }
-        public string Category { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "must be a positive value.")] //solo se puede agregar un numero mayor o igual a 0
         public int Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "must be a positive value.")]
         public int Stock { get; set; }
+
+        //de uno a muchos, Pero no estoy seguro si esto esta bien.
+        [ForeignKey ("Reservation")]
+        public int? ReservationId {  get; set; }
+
+        public Reservation Reservation { get; set; }
     }
 }
