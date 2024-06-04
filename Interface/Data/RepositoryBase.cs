99c1ca055c1552c1e12cbeed6f233c6e1061644e
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-06-04 20:02:16 -0300

    Add repository

---
+++ Interface/Data/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public abstract class RepositoryBase<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+        public RepositoryBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return _dbContext.Set<T>().ToList();
+        }
+
+        public virtual T GetById<TId>(TId id) 
+        { 
+            return _dbContext.Set<T>().Find(new object[] { id });
+        }
+
+        public virtual T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public virtual void Update(T entity)
+        {
+           _dbContext.Set<T>().Update(entity);
+           _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Interface/Data/ReservationRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ReservationRepository : RepositoryBase<Reservation>
+    {
+        private readonly ApplicationContext _context;
+        public ReservationRepository(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        /*
+        public ICollection<Sneaker> GetSneaker
+
+        public ICollection<Subject> GetStudentSubjects(int studentId) =>
+        _context.Students.Include(a => a.SubjectsAttended).ThenInclude(m => m.Professors).Where(a => a.Id == studentId)
+        .Select(a => a.SubjectsAttended).FirstOrDefault() ?? new List<Subject>();*/
+    }
+}
